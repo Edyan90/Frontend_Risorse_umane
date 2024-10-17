908105d2d41e677aa350df34fc643fe98431dd64
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-10-17 18:46:55 +0200

    Update index.js

--- frontend-capstone/src/redux/store/index.js
+++ frontend-capstone/src/redux/store/index.js
@@ -1,8 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
-import mainReducer from "../reducers";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import mainReducer from "../reducers/mainReducer";
+import dipendenteReducer from "../reducers/dipendenteReducer";
+import themeReducer from "../reducers/themeReducer";
+import listaDipendentiReducer from "../reducers/listaDipendentiReducer";
+import listaBustePagaReducer from "../reducers/listaBustePagaReducer";
+import listaFerieReducer from "../reducers/listaFerieReducer";
+import listaAssenzeReducer from "../reducers/listaAssenzeReducer";
+import listaPresenzeReducer from "../reducers/listaPresenzeReduce";
 
+const bigReducer = combineReducers({
+  main: mainReducer,
+  dipendente: dipendenteReducer,
+  theme: themeReducer,
+  listaDipendenti: listaDipendentiReducer,
+  listaFerie: listaFerieReducer,
+  listaBustePaga: listaBustePagaReducer,
+  listaAssenze: listaAssenzeReducer,
+  listaPresenze: listaPresenzeReducer,
+});
 const store = configureStore({
-  reducer: mainReducer,
+  reducer: bigReducer,
 });
 
 export default store;
