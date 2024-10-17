1ded83fdeeb0ba7911f797f5a111d1cddf0d930c
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-10-17 18:46:07 +0200

    Update Index.jsx

--- frontend-capstone/src/components/Index.jsx
+++ frontend-capstone/src/components/Index.jsx
@@ -1,10 +1,21 @@
 import { Col, Container, Row } from "react-bootstrap";
 import Login from "./Login";
 import ThemeButton from "./ThemeButton";
+import MyFooter from "./MyFooter";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const Index = () => {
+  const darkTheme = useSelector((state) => state.theme.darkTheme);
+  useEffect(() => {
+    if (darkTheme) {
+      document.body.classList.add("bg-dark", "text-light");
+    } else {
+      document.body.classList.remove("bg-dark", "text-light");
+    }
+  }, [darkTheme]);
   return (
-    <Container style={{ height: "100vh" }}>
+    <Container style={{ height: "100vh" }} className="text-black">
       <Row className="justify-content-start border-bottom mb-5 text-center align-items-center">
         <Col className="text-start">
           <img
@@ -16,19 +27,18 @@ const Index = () => {
           />
         </Col>
         <Col className="text-end">
-          <ThemeButton />
+          <ThemeButton darkTheme={darkTheme} />
         </Col>
       </Row>
 
       <Row className=" border-bottom pb-5">
-        <Col>
-          <h1 className="text-start">Benvenuto HR Portal</h1>
+        <Col className="d-flex justify-content-center">
           <Login />
         </Col>
         <Col>
           <img
             src="https://dhamzv23wqk2g.cloudfront.net/wp-content/uploads/2023/06/05170126/HR-Management.jpg"
-            className="img-fluid"
+            className="img-fluid rounded-3"
           />
         </Col>
       </Row>
@@ -39,20 +49,7 @@ const Index = () => {
         </p>
         <p>L &apos;accesso non autorizzato è severamente proibito</p>
       </Row>
-      <Row className="">
-        <Col>
-          <div className="d-flex text-center align-items-center">
-            <a>
-              <img
-                src="https://saas.hrzucchetti.it/hrpbestengsrl/images/login/logo.svg"
-                width={75}
-                className="img-fluid px-2"
-              />
-            </a>
-            <p className="my-0"> ©2024 Epicode HR, Epicode. All rights reserved. HRPortal </p>
-          </div>
-        </Col>
-      </Row>
+      <MyFooter />
     </Container>
   );
 };
