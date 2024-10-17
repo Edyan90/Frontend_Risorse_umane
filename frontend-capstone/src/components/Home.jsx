f11b106e356cd0c3ffd23526806e1e7a858a0f23
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-10-17 18:46:04 +0200

    Update Home.jsx

--- frontend-capstone/src/components/Home.jsx
+++ frontend-capstone/src/components/Home.jsx
@@ -1,4 +1,52 @@
+import { Col, Row } from "react-bootstrap";
+
+import DashBoard from "./DashBoard";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setDipendente } from "../redux/actions";
+
 const Home = () => {
-  return <></>;
+  const darkTheme = useSelector((state) => state.theme.darkTheme);
+  const dispatch = useDispatch();
+  const dipendente = useSelector((state) => state.dipendente);
+  const dipendenteFetch = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const resp = await fetch("http://localhost:3001/dipendenti/me", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+      if (!resp.ok) {
+        throw new Error("Errore nella fetch");
+      }
+      const data = await resp.json();
+      dispatch(setDipendente(data));
+      console.log(data);
+    } catch (error) {
+      console.error("Errore nella fetch dipendente: ", error);
+    }
+  };
+  useEffect(() => {
+    dipendenteFetch();
+    console.log(dipendente);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  useEffect(() => {
+    if (darkTheme) {
+      document.body.classList.add("bg-dark", "text-light");
+    } else {
+      document.body.classList.remove("bg-dark", "text-light");
+    }
+  }, [darkTheme]);
+  return (
+    <Row>
+      <Col>
+        <DashBoard darkTheme={darkTheme} dipendente={dipendente} />
+      </Col>
+    </Row>
+  );
 };
 export default Home;
