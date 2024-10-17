89fef3b179d7e0742bcc2bee1f6d3e3e971e0165
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-10-17 18:45:42 +0200

    Update App.jsx

--- frontend-capstone/src/App.jsx
+++ frontend-capstone/src/App.jsx
@@ -3,14 +3,33 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./components/Index";
 import Home from "./components/Home";
+import NotFound from "./components/NotFound";
+import { useSelector } from "react-redux";
+import Layout from "./components/Layout";
+import Dipendenti from "./components/Dipendenti";
+import Ferie from "./components/Ferie";
+import BustePaga from "./components/BustePaga";
+import Presenze from "./components/Presenze";
+import Assenze from "./components/Assenze";
+import Profile from "./components/Profile";
 
 function App() {
+  const darkTheme = useSelector((state) => state.theme.darkTheme);
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/home" element={<Home />} />
+          <Route element={<Layout darkTheme={darkTheme} />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/dipendenti" element={<Dipendenti />} />
+            <Route path="/ferie" element={<Ferie />} />
+            <Route path="/bustepaga" element={<BustePaga />} />
+            <Route path="/presenze" element={<Presenze />} />
+            <Route path="/assenze" element={<Assenze />} />
+            <Route path="/dipendenti/:dipendenteID" element={<Profile />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
