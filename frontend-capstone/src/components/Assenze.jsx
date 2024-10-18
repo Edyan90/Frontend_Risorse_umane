dde0dbe69e1d5379a914fb0f33ab76859424f12b
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-10-18 12:30:56 +0200

    filtri per assenze

--- frontend-capstone/src/components/Assenze.jsx
+++ frontend-capstone/src/components/Assenze.jsx
@@ -1,16 +1,23 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-
-import { Button, Col, Row, Table } from "react-bootstrap";
+import { Button, Col, Form, Row, Table } from "react-bootstrap";
 import { setListAssenze } from "../redux/actions";
 
 const Assenze = () => {
   const dipendente = useSelector((state) => state.dipendente.dipendente);
   const lista = useSelector((state) => state.listaAssenze.listaAssenze);
+  console.log(lista);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
+  const [listaCompleta, setListaCompleta] = useState([]);
+  const [filtri, setFiltri] = useState({
+    nome: "",
+    cognome: "",
+    assenzaID: "",
+    stato: "",
+  });
 
   const listaAssenze = async () => {
     try {
@@ -22,11 +29,12 @@ const Assenze = () => {
         },
       });
       if (!resp.ok) {
-        throw new Error("Errore nella fetch lista Bustepaga");
+        throw new Error("Errore nella fetch lista assenze");
       }
       const data = await resp.json();
       console.log("dati ricevuti", data.content);
       dispatch(setListAssenze(data.content));
+      setListaCompleta(data.content);
     } catch (error) {
       console.error("Errore nella fetch lista assenze", error);
     } finally {
@@ -34,14 +42,38 @@ const Assenze = () => {
     }
   };
 
+  const handleFilterChange = (e) => {
+    const { name, value } = e.target;
+    setFiltri((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const filtraDipendenti = () => {
+    return listaCompleta.filter((dipendente) => {
+      if (!dipendente.assenze || dipendente.assenze.length === 0) {
+        return false;
+      }
+      const assenzeFiltrate = dipendente.assenze.filter((assenza) => {
+        return (
+          (filtri.assenzaID === "" || assenza.id.toString().toLowerCase().includes(filtri.assenzaID.toLowerCase())) &&
+          (filtri.stato === "" || assenza.stato.toLowerCase().includes(filtri.stato.toLowerCase()))
+        );
+      });
+      const nomefiltrato = filtri.nome === "" || dipendente.nome.toLowerCase().includes(filtri.nome.toLowerCase());
+      const cognomefiltrato =
+        filtri.cognome === "" || dipendente.cognome.toLowerCase().includes(filtri.cognome.toLowerCase());
+      return assenzeFiltrate.length > 0 && nomefiltrato && cognomefiltrato;
+    });
+  };
+
   useEffect(() => {
     listaAssenze();
-    console.log("ciao", lista);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const dipendentiFiltrati = filtraDipendenti();
+
   return (
-    <Row>
+    <>
       {loading ? (
         <Col>
           <p>Caricamento...</p>
@@ -82,53 +114,109 @@ const Assenze = () => {
             <Button onClick={() => navigate("/home")}>Torna alla Home</Button>
           </Col>
         )
-      ) : lista && lista.length > 0 ? (
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Dipendente</th>
-              <th>Assenza ID</th>
-              <th>Data</th>
-              <th>Motivo</th>
-              <th>Stato</th>
-              <th>Azioni</th>
-            </tr>
-          </thead>
-          <tbody>
-            {lista.map((dipendente) =>
-              dipendente.assenze.length > 0 ? (
-                dipendente.assenze.map((assenza) => (
-                  <tr key={assenza.id}>
-                    <td>
-                      {dipendente.nome} {dipendente.cognome}
-                    </td>
-                    <td>{assenza.id}</td>
-                    <td>{assenza.data}</td>
-                    <td>{assenza.motivo}</td>
-                    <td>{assenza.stato}</td>
-                    <td>
-                      <Button onClick={() => navigate(`/dipendenti/${dipendente.id}`)}>Approva</Button>
-                    </td>
-                  </tr>
-                ))
-              ) : (
-                <tr key={dipendente.id}>
-                  <td>
-                    {dipendente.nome} {dipendente.cognome}
-                  </td>
-                  <td colSpan="5">Nessuna assenza registrata</td>
-                </tr>
-              )
-            )}
-          </tbody>
-        </Table>
       ) : (
-        <Col>
-          <p>Nessuna assenza registrata!</p>
-          <Button onClick={() => navigate(`/dipendenti/${dipendente.id}`)}>Vedi profilo</Button>
-        </Col>
+        <>
+          <Row className="mb-3">
+            <h3 className="text-start text-light">Ricerca per:</h3>
+            <Form className="d-flex gap-2 justify-content-start">
+              <Form.Group controlId="nome">
+                <Form.Control
+                  placeholder="@nome dipendente"
+                  type="text"
+                  name="nome"
+                  value={filtri.nome}
+                  onChange={handleFilterChange}
+                  className="custom-input"
+                />
+              </Form.Group>
+              <Form.Group controlId="cognome">
+                <Form.Control
+                  placeholder="@cognome dipendente"
+                  type="text"
+                  name="cognome"
+                  value={filtri.cognome}
+                  onChange={handleFilterChange}
+                  className="custom-input"
+                />
+              </Form.Group>
+              <Form.Group controlId="assenzaID">
+                <Form.Control
+                  placeholder="@assenzaID"
+                  type="text"
+                  name="assenzaID"
+                  value={filtri.assenzaID}
+                  onChange={handleFilterChange}
+                  className="custom-input"
+                />
+              </Form.Group>
+              <Form.Group controlId="stato">
+                <Form.Control
+                  as="select"
+                  name="stato"
+                  value={filtri.stato}
+                  onChange={handleFilterChange}
+                  className="custom-input"
+                >
+                  <option value="">Seleziona Stato</option>
+                  <option value="APPROVATO">APPROVATO</option>
+                  <option value="NON_APPROVATA">NON APPROVATA</option>
+                  <option value="IN_ATTESA">IN ATTESA</option>
+                </Form.Control>
+              </Form.Group>
+            </Form>
+          </Row>
+          <Row>
+            {dipendentiFiltrati && dipendentiFiltrati.length > 0 ? (
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>Dipendente</th>
+                    <th>Assenza ID</th>
+                    <th>Data</th>
+                    <th>Motivo</th>
+                    <th>Stato</th>
+                    <th>Azioni</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {dipendentiFiltrati.map((dipendente) =>
+                    dipendente.assenze.length > 0 ? (
+                      dipendente.assenze.map((assenza) => (
+                        <tr key={assenza.id}>
+                          <td>
+                            {dipendente.nome} {dipendente.cognome}
+                          </td>
+                          <td>{assenza.id}</td>
+                          <td>{assenza.data}</td>
+                          <td>{assenza.motivo}</td>
+                          <td>{assenza.stato}</td>
+                          <td>
+                            <Button onClick={() => navigate(`/dipendenti/${dipendente.id}`)}>Approva</Button>
+                          </td>
+                        </tr>
+                      ))
+                    ) : (
+                      <tr key={dipendente.id}>
+                        <td>
+                          {dipendente.nome} {dipendente.cognome}
+                        </td>
+                        <td colSpan="5">Nessuna assenza registrata</td>
+                      </tr>
+                    )
+                  )}
+                </tbody>
+              </Table>
+            ) : (
+              <Col>
+                <h1>Nessuna assenza registrata!</h1>
+                <Button onClick={() => navigate(`/home`)}>Torna alla Home</Button>
+              </Col>
+            )}
+          </Row>
+        </>
       )}
-    </Row>
+    </>
   );
 };
+
 export default Assenze;
